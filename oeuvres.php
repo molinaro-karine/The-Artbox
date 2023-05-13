fa5aa2f3c3933720b1937bc94da7a6217cabb03d
molinaro karine <molinaro.karine@hotmail.com>
2023-05-13 15:38:51 +0200

    tableau associatif

---
+++ oeuvres.php
@@ -0,0 +1,94 @@
+<?php
+$oeuvres = [
+    [
+        'id' => 0,
+        'image' => '<img src="img/clark-van-der-beken.png" alt="Dodomu">',
+        'title' => 'Dodomu',
+        'description' => 'Mia Tozerski',
+    ],
+    [
+        'id' => 1,
+        'image' => '<img src="img/pawel-czerwinski-3.png" alt="Aashaaheen Baadal">',
+        'title' => 'Aashaaheen Baadal',
+        'description' => 'Anaisha Devi',
+    ],
+    [
+        'id' => 2,
+        'image' => '<img src="img/dan-cristian-padure.png" alt="Nightlife Traffic">',
+        'title' => 'Nightlife Traffic',
+        'description' => 'Andrew Forsythe',
+    ],
+    [
+        'id' => 3,
+        'image' => '<img src="img/steve-johnson-5.png" alt="Le refuge de l\'Havre">',
+        'title' => 'Le refuge de l\'Havre',
+        'description' => 'Simon Pelletier',
+    ],
+    [
+        'id' => 4,
+        'image' => ' <img src="img/steve-johnson.png" alt="Red Washover">',
+        'title' => 'Red Washover',
+        'description' => 'Kit Van Der Borght',
+    ],
+    [
+        'id' => 5,
+        'image' => ' <img src="img/pawel-czerwinski.png" alt="Chromatics">',
+        'title' => 'Chromatics',
+        'description' => 'Jean-Michel Delatronchette',
+    ],
+    [
+        'id' => 6,
+        'image' => '<img src="img/jazmin-quaynor.png" alt="Digital Negative">',
+        'title' => 'Digital Negative',
+        'description' => 'Hamish McKee',
+    ],
+    [
+        'id' => 7,
+        'image' => '<img src="img/steve-johnson-6.png" alt="Blast from the past">',
+        'title' => 'Blast from the past',
+        'description' => 'Juliette Baskerville',
+    ],
+    [
+        'id' => 8,
+        'image' => ' <img src="img/victor-grabarczyk.png" alt="Hurricane">',
+        'title' => 'Hurricane',
+        'description' => 'Natalie Wellington',
+    ],
+    [
+        'id' => 9,
+        'image' => ' <img src="img/pawel-czerwinski-2.png" alt="La marée rouge">',
+        'title' => 'La marée rouge',
+        'description' => 'Martin Rodriguez',
+    ],
+    [
+        'id' => 10,
+        'image' => ' <img src="img/steve-johnson-2.png" alt="Asimilacion">',
+        'title' => 'Asimilacion',
+        'description' => 'Angel Sanchez-Fernandez',
+    ],
+    [
+        'id' => 11,
+        'image' => ' <img src="img/fly-d.png" alt="La Galaxia Gialla">',
+        'title' => 'La Galaxia Gialla',
+        'description' => 'Eduardo Tancredi',
+    ],
+    [
+        'id' => 12,
+        'image' => ' <img src="img/orfeas-green.png" alt="Puffy Amalgamate">',
+        'title' => 'Puffy Amalgamate',
+        'description' => 'Sandro De Blasi',
+    ],
+    [
+        'id' => 13,
+        'image' => '<img src="img/steve-johnson-4.png" alt="Mirage">',
+        'title' => 'Mirage',
+        'description' => 'Stéphanie Kaiser',
+    ],
+    [
+        'id' => 14,
+        'image' => ' <img src="img/steve-johnson-3.png" alt="Blaue Gelbe Muster">',
+        'title' => 'Blaue Gelbe Muster',
+        'description' => 'Adelheid Von Schreiber',
+    ],
+];
+?>
